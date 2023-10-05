f5087b0a85db71439b81c634ec51a442d0a5ab7d
Parlog07 <ayoubmogador2014@gmail.com>
2023-10-05 21:21:08 +0100

    update the task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-#  Prints the number of and the list of its arguments
 if __name__ == "__main__":
     import sys
+    i = len(sys.argv) - 1
 
-    arg = sys.argv
-    size = len(arg) - 1
-
-    if size > 1:
-        print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
-
-    elif size == 0:
-        print("{} arguments.".format(size))
-
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    # Print sorted name from directory
-    for name in sorted(dir(hidden_4)):
-        # print only names that do not start with __
-        if name[:2] != '__':
-            print("{}".format(name))
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
